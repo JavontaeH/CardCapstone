9da8fcf58d4b8eaa9934d93bad1de0ef764e6320
JavontaeH <hardeman.javontae@gmail.com>
2022-07-25 16:07:50 -0500

    fixed card data bug

--- CardCapstone/client/src/components/collection/CollectionCard.js
+++ CardCapstone/client/src/components/collection/CollectionCard.js
@@ -1,13 +1,13 @@
-export const CollectionCard = (card) => {
+export const CollectionCard = ({ card }) => {
   console.log(card);
   return (
     <div className="collection-card-wrapper">
-      <h1>{card.name}</h1>
-      <h3>{card.description}</h3>
-      <h5>{card.atk}</h5>
-      <h5>{card.hp}</h5>
-      <h5>{card.mana}</h5>
-      <img src={card.imageLocation} alt="card-image" />
+      <h1>{card?.name}</h1>
+      <h3>{card?.description}</h3>
+      <h5>{card?.atk}</h5>
+      <h5>{card?.hp}</h5>
+      <h5>{card?.mana}</h5>
+      {/* <img src={card?.imageLocation} alt="card-image" /> */}
     </div>
   );
 };
