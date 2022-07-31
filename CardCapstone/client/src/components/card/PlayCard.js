c0b34e17c5ec5523e3bc3f6c1010851ed1f2becd
JavontaeH <hardeman.javontae@gmail.com>
2022-07-31 14:19:25 -0500

    updated frame svg to accept dynamic cards

--- CardCapstone/client/src/components/card/PlayCard.js
+++ CardCapstone/client/src/components/card/PlayCard.js
@@ -3,10 +3,10 @@ import { RenderFrame, Frame, Image, Strength, Health } from "./RenderFrame";
 export const PlayCard = ({ card }) => {
   return (
     <RenderFrame>
-      <Image id="/images/card-art/Chillwind_Yeti.png" clip />
+      <Image id={card.imageLocation} clip />
       <Frame />
-      <Health fontFamily="Belwe">1</Health>
-      <Strength fontFamily="Belwe">1</Strength>
+      <Health fontFamily="Belwe">{card.hp}</Health>
+      <Strength fontFamily="Belwe">{card.atk}</Strength>
     </RenderFrame>
   );
 };
